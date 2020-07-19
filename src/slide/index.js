c3bb9c73aca0140d165ea3cc27347d257dbd4cb3
Edgard <ed.leo@hotmail.com>
2020-07-19 19:56:41 -0300

    novo hover e skeleton

--- src/slide/index.js
+++ src/slide/index.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import Slider from "react-slick";
 import {Container} from 'react-bootstrap';
+import Skeleton from 'react-loading-skeleton';
+
 import './styles.scss';
-import Load from '../load';
 
 function Slide() {
   const [loading, setLoading] = useState(true);
@@ -15,35 +16,46 @@ function Slide() {
     }
 
     useEffect(() => {
-      async function top() {
-          const top = await  window.scrollTo(0, 0);
-          setLoading(false)
-          return top;
+      async function _Load() {
+          window.scrollTo(0, 0);
+          setTimeout(() => {
+            setLoading(false)
+          }, 2000)
       }
-      top();
+      _Load();
   }, []);
 
   return (
     <>
-    {loading ? <Load /> : false}
+   
     <div className="section-slider">
         <h2><small>especial para você</small>produtos selecionados</h2>
     <Container>
     <Slider {...settings}>
       <div className="product-holder">
+      {loading ? <Skeleton height={294} width={194} /> :
         <div className="product-thumb" style={{backgroundImage: "url('https://media.edega.com.br/QWQBWEba.png')"}}></div>
+      }
       </div>
       <div className="product-holder">
+      {loading ? <Skeleton height={294} width={194} /> :
         <div className="product-thumb" style={{backgroundImage: "url('https://media.edega.com.br/q9Awf8Pu.png')"}}></div>
+      }
       </div>
       <div className="product-holder">
+      {loading ? <Skeleton height={294} width={194} /> : 
         <div className="product-thumb" style={{backgroundImage: "url('https://media.edega.com.br/j1sbC6J5.png')"}}></div>
+      }
       </div>
       <div className="product-holder">
+      {loading ? <Skeleton height={294} width={194} /> : 
         <div className="product-thumb" style={{backgroundImage: "url('https://media.edega.com.br/Xr70F7Bx.png')"}}></div>
-      </div>
+      }
+        </div>
       <div className="product-holder">
+      {loading ? <Skeleton height={294} width={194} /> : 
         <div className="product-thumb" style={{backgroundImage: "url('https://media.edega.com.br/QWQBWEba.png')"}}></div>
+      }
       </div>
       <div className="product-holder">
         <div className="product-thumb" style={{backgroundImage: "url('https://media.edega.com.br/q9Awf8Pu.png')"}}></div>
--- src/slideGrow/index.js
+++ src/slideGrow/index.js
@@ -1,28 +1,41 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import Skeleton from 'react-loading-skeleton';
 import './styles.scss';
 
 function SlideGrow() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function _Load() {
+            setTimeout(() => {
+              setLoading(false)
+            }, 2000)
+        }
+        _Load();
+    }, []);
+
   return (
       <>
-       <div className="hero-home__slide">
-          <div className="slide-text">
-              <span className="eyebrow">PROMOÇÃO DIVA BOUTEILLE</span> 
-              <header className="section__head">
-                  <h2 className="section__title"></h2>
-              </header>
-              <p className="section__p"></p>
-              <a href="" className="btn-text">
-              <span>Confira!</span>
-              <i className="ico-dot"></i>
-              </a>
-           </div>
-           <div className="hero-img img-hover-zoom">
-               <a href="">
-                   <img src="https://media.edega.com.br/VwFbsFdb.jpg" alt="home hero"  />
+        {loading ? <Skeleton height={500} width={'100%'} /> :
+        <div className="hero-home__slide">
+            <div className="slide-text">
+                <span className="eyebrow">PROMOÇÃO DIVA BOUTEILLE</span> 
+                <header className="section__head">
+                    <h2 className="section__title"></h2>
+                </header>
+                <p className="section__p"></p>
+                <a href="" className="btn-text">
+                <span>Confira!</span>
+                <i className="ico-dot"></i>
                 </a>
             </div>
+            <div className="hero-img img-hover-zoom">
+                <a href="">
+                    <img src="https://media.edega.com.br/VwFbsFdb.jpg" alt="home hero"  />
+                    </a>
+            </div>
         </div>
+        }
       </>
   );
 }
